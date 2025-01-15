d18338128110886b7890656c33dfb2c8aedc72e0
yakim <kya050102@gmail.com>
2025-01-15 16:57:35 +0900

    first commit

--- lib/screen/image/image_screen.dart
+++ lib/screen/image/image_screen.dart
@@ -4,7 +4,7 @@ class ImageScreen extends StatelessWidget {
   const ImageScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context) {  
     return Scaffold(
       appBar: AppBar(
         title: Text("Image"),
